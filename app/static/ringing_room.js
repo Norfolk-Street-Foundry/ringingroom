a869ad31954b1bff2917d72a1f0ac4f4858b2dcc
Jonathan Agg <jonathan.agg@gmail.com>
2020-11-29 18:58:27 +0000

    Avoid breaking other numbers of bells.

--- app/static/ringing_room.js
+++ app/static/ringing_room.js
@@ -380,6 +380,9 @@ $(document).ready(function() {
             },
             
             getLabel: function(number) {
+              if (this.number_of_bells < 17) {
+                return number;
+              }
               var names = ['1','2','3','4','5','6#','6','7','8','9','10','11','12','13','14','15','16'];
               return names[number - 1];
             },
